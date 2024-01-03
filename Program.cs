58034c695378ae00e697f6816426de9c6eed466b
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-01-02 20:29:32 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GA_WriteLine_WCram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.Write("Edsger W. Dijkstra:");
+            Console.WriteLine(" Computer science is no more about computers than astronomy is about telescopes.");
+            Console.WriteLine();
+            Console.Write("Alan Kay:");
+            Console.WriteLine("  The best way to predict the future is to invent it.");
+            
+        }
+
+    }
+}
